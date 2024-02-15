a6d10462bfef11c69b97dcaf064b4c592dbbf458
Jordan Duran <jordanduranm@gmail.com>
2024-02-15 12:04:10 -0500

    Setup Vue app, initial Events and reactivity exposure

---
+++ src/components/Timeline.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+const periods = ['Today', 'This Week', 'This Month'];
+
+const selectedPeriod = ref('Today');
+
+function handleSelectPeriod(period: string) {
+  console.log(period);
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+      <!-- Looping in Vue -->
+      <a v-for='period of periods' :key="period" @click="handleSelectPeriod(period)">
+        <!-- :key is shorthand for v-bind:key -->
+        {{ period }}</a>
+    </span>
+  </nav>
+</template>
