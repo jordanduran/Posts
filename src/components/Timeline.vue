81ddb6530c1e3c57c06fe71a18205424895f125f
Jordan Duran <jordanduranm@gmail.com>
2024-02-15 12:55:59 -0500

    Derived data with computed

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,5 +1,7 @@
 <script setup lang='ts'>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { Post, today, thisWeek, thisMonth } from '../posts'
+import { DateTime } from 'luxon'
 
 const periods = ['Today', 'This Week', 'This Month'] as const;
 
@@ -10,6 +12,25 @@ const selectedPeriod = ref<Period>('Today');
 function handleSelectPeriod(period: Period) {
   selectedPeriod.value = period;
 }
+
+const posts = computed(() => {
+  return [today, thisWeek, thisMonth].map(post => {
+    return {
+      ...post,
+      created: DateTime.fromISO(post.created)
+    }
+  }).filter(post => {
+    if (selectedPeriod.value === 'Today') {
+      return post.created >= DateTime.now().minus({ day: 1 })
+    }
+
+    if (selectedPeriod.value === 'This Week') {
+      return post.created >= DateTime.now().minus({ week: 1 })
+    }
+
+    return post
+  })
+})
 </script>
 
 <template>
@@ -22,5 +43,9 @@ function handleSelectPeriod(period: Period) {
         <!-- :key is shorthand for v-bind:key -->
         {{ period }}</a>
     </span>
+    <a v-for="post of posts" :key="post.id" class="panel-block">
+      <a>{{ post.title }}</a>
+      <div>{{ post.created.toFormat('d MMM') }}</div>
+    </a>
   </nav>
 </template>
