027e345aec02699f07dc25450af600e914a4833e
Jordan Duran <jordanduranm@gmail.com>
2024-02-16 10:45:11 -0500

    Creating PostWriter component

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { TimelinePost } from '../posts';
+
+defineProps<{
+  post: TimelinePost
+}>()
+</script>
+
+<template>
+  <div class="div columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post Title</div>
+        <input type="text" class="input" />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+import { DateTime } from 'luxon'
+import PostWriter from '../components/PostWriter.vue'
+import { TimelinePost } from '../posts.ts'
+const post: TimelinePost = {
+  id: '-1',
+  title: 'Title 1',
+  created: DateTime.now()
+}
+</script>
+
 <template>
   New Post
+  <PostWriter :post="post" />
 </template>
