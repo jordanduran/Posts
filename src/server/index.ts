7b7a26711c77c58ddd07e09fa53c12b2be8f2820
Jordan Duran <jordanduranm@gmail.com>
2024-02-16 12:45:09 -0500

    POST endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-import { today, thisWeek, thisMonth } from '../posts';
+import bodyParser from 'body-parser';
+import { today, thisWeek, thisMonth, Post } from '../posts';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser);
+
+const allPosts = [today, thisWeek, thisMonth];
 
 app.get('/posts', (req, res) => {
   res.json([today, thisWeek, thisMonth]);
+});
+
+app.post<{}, {}, Post>('/posts', (req, res) => {
+  const post = { ...req.body, id: (Math.random() * 10000).toFixed() };
+  allPosts.push(post);
+  res.json(post);
 });
 
 app.listen(8000, () => {
